38fce2315d953db44928cc576d62671598a5382d
josequesta <questaschindlerjose@gmail.com>
2020-10-26 03:15:29 -0300

    Revert "decimales"
    This reverts commit 8ae7b8ebdae229da415cecfc93797926f3bbe071.

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function calcularcosto() {
     var dolaroficial = document.getElementById("dolaroficial").value;
     var dolarblue = document.getElementById("dolarblue").value;
     var precioproducto = document.getElementById("precioproducto").value;    
-    var precioars = parseInt(dolaroficial)*parseInt(precioproducto); 
-    var arsconDecimal = precioars.toFixed(2); 
-    document.getElementById("precioenars").innerHTML = arsconDecimal;
+    var precioars = parseInt(dolaroficial)*parseInt(precioproducto);
+    document.getElementById("precioenars").innerHTML = precioars;
     var precioblue = parseInt(precioars)/parseInt(dolarblue);
-    var blueconDecimal = precioblue.toFixed(2); 
-    document.getElementById("precioenblue").innerHTML = blueconDecimal;
+    document.getElementById("precioenblue").innerHTML = precioblue;
 }
