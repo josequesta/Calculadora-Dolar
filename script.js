3a8cb44fdd57888c96cce111d457651ff3665df4
josequesta <questaschindlerjose@gmail.com>
2020-10-29 12:44:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ function calcularcosto() {
     var dolaroficial = document.getElementById("dolaroficial").value;
     var dolarblue = document.getElementById("dolarblue").value;
     var precioproducto = document.getElementById("precioproducto").value;    
-    var precioars = parseInt(dolaroficial)*parseInt(precioproducto); 
+    var precioars = dolaroficial*precioproducto; 
     var arsconDecimal = precioars.toFixed(2); 
     document.getElementById("precioenars").innerHTML = arsconDecimal;
-    var precioblue = parseInt(precioars)/parseInt(dolarblue);
+    var precioblue = precioars/dolarblue;
     var blueconDecimal = precioblue.toFixed(2); 
     document.getElementById("precioenblue").innerHTML = blueconDecimal;
 }
+document.addEventListener("keydown", event => {
+    if (event.key == "Enter") {
+        calcularcosto();
+    }
+}, true);
